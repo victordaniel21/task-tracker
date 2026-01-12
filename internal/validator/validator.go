b3c825cf3ab67ebd7807c0e48bd8cfcb0abbbabd
victordaniel21 <victordev@GoDev.localdomain>
2026-01-12 14:23:56 +0800

    add validator.go

---
+++ internal/validator/validator.go
@@ -0,0 +1,10 @@
+package validator
+
+// IsEmailValid checks if the string is a valid email.
+// 🧠 WHY CAPITALS? In Go, if a function name starts with a Capital Letter,
+// it is Exported (Public). If it starts with lowercase, it is Private
+// and can only be seen inside the 'validator' package.
+func IsEmailValid(email string) bool {
+	// We will add real logic later.
+	return true
+}
